59bf2d478917f00a637ddefafc054a6d20965f9b
Mohamed Abdelaziz <114187241+Mohamed-192002@users.noreply.github.com>
2025-02-12 19:12:16 +0200

    add and edit emp

--- StaffWork.Web/Controllers/EmployeeController.cs
+++ StaffWork.Web/Controllers/EmployeeController.cs
@@ -1,86 +1,149 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using StaffWork.Api.Controllers;
+using StaffWork.Core.Consts;
 using StaffWork.Core.Interfaces;
 using StaffWork.Core.Models;
 using StaffWork.Core.Paramaters;
 using StaffWork.Infrastructure.Filters;
+using StaffWork.Infrastructure.Implementations;
+using System.Security.Claims;
 
 namespace StaffWork.Web.Controllers
 {
-	public class EmployeeController : ApiBaseController<Employee>
-	{
-		public EmployeeController(IServicesBase<Employee> servicesBase, IMapper mapper)
-			: base(servicesBase, mapper)
-		{
-		}
+    public class EmployeeController : ApiBaseController<Employee>
+    {
+        public readonly IServicesBase<User> UserService;
 
-		[HttpGet]
-		public async Task<IActionResult> Index()
-		{
-			var model = await BussinesService.GetAllAsync();
-			var viewModel = _mapper.Map<IEnumerable<EmployeeViewModel>>(model);
+        public EmployeeController(IServicesBase<Employee> servicesBase, IMapper mapper, IServicesBase<User> userService)
+            : base(servicesBase, mapper)
+        {
+            UserService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var model = await BussinesService.GetAllAsync();
+            var viewModel = _mapper.Map<IEnumerable<EmployeeViewModel>>(model);
 
             var model1 = new Tuple<IEnumerable<EmployeeViewModel>, EmployeeViewModel>(
                             viewModel, new EmployeeViewModel());
 
             return View(model1);
-		}
-		[HttpPost]
-		public async Task<IActionResult> Create(EmployeeViewModel viewModel)
-		{
-			if (!ModelState.IsValid)
-				return BadRequest(ModelState);
-			var Employee = _mapper.Map<Employee>(viewModel);
-			await BussinesService.InsertAsync(Employee);
-		   return RedirectToAction("Index", _mapper.Map<EmployeeViewModel>(Employee));
-		}
-		[HttpGet]
-		[AjaxOnly]
-		public async Task<IActionResult> EditAsync(int id)
-		{
-			var Employee = await BussinesService.GetAsync(d => d.Id == id);
-			if (Employee == null)
-				return NotFound();
-			var viewModel = _mapper.Map<EmployeeViewModel>(Employee);
-			return PartialView("_FormEdit", viewModel);
-		}
-		[HttpPost]
-		[AutoValidateAntiforgeryToken]
-		public async Task<IActionResult> EditAsync(EmployeeViewModel viewModel)
-		{
-			if (!ModelState.IsValid)
-				return BadRequest();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(EmployeeViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var Employee = _mapper.Map<Employee>(viewModel);
+            await BussinesService.InsertAsync(Employee);
+            return RedirectToAction("Index", _mapper.Map<EmployeeViewModel>(Employee));
+        }
+        [HttpGet]
+        [AjaxOnly]
+        public async Task<IActionResult> EditAsync(int id)
+        {
+            var Employee = await BussinesService.GetAsync(d => d.Id == id);
+            if (Employee == null)
+                return NotFound();
+            var viewModel = _mapper.Map<EmployeeViewModel>(Employee);
+            return PartialView("_FormEdit", viewModel);
+        }
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> EditAsync(EmployeeViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
             var Employee = await BussinesService.GetAsync(d => d.Id == viewModel.Id);
             if (Employee == null)
                 return NotFound();
             _mapper.Map(viewModel, Employee);
 
             await BussinesService.UpdateAsync(Employee.Id, Employee);
-			return RedirectToAction("Index", _mapper.Map<EmployeeViewModel>(Employee));
-		}
-		public async Task<IActionResult> Delete(int id)
-		{
-			var Employee = await BussinesService.GetAsync(d => d.Id == id);
-			if (Employee == null)
-				return NotFound();
-			try
-			{
-				await BussinesService.DeleteAsync(id);
-			}
-			catch (Exception)
-			{
-				throw;
-			}
-			return Ok();
-		}
-		public async Task<IActionResult> AllowItem(EmployeeViewModel model)
-		{
-			var Employee = await BussinesService.GetAsync(d => d.FullName == model.FullName);
+            return RedirectToAction("Index", _mapper.Map<EmployeeViewModel>(Employee));
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            var Employee = await BussinesService.GetAsync(d => d.Id == id);
+            if (Employee == null)
+                return NotFound();
+            try
+            {
+                await BussinesService.DeleteAsync(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return Ok();
+        }
+        public async Task<IActionResult> AllowItem(EmployeeViewModel model)
+        {
+            var Employee = await BussinesService.GetAsync(d => d.FullName == model.FullName);
 
-			var isAllowed = Employee is null || Employee.Id.Equals(model.Id);
+            var isAllowed = Employee is null || Employee.Id.Equals(model.Id);
 
-			return Json(isAllowed);
-		}
-	}
+            return Json(isAllowed);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetEmployees()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (string.IsNullOrEmpty(userId))
+                return Unauthorized();
+            var user = await UserService.GetAsync(x => x.Id == userId);
+
+            var skip = int.Parse(Request.Form["start"]!);
+            var pageSize = int.Parse(Request.Form["length"]!);
+            var searchValue = Request.Form["search[value]"];
+            var sortColumnIndex = Request.Form["order[0][column]"];
+            var sortColumn = Request.Form[$"columns[{sortColumnIndex}][name]"];
+            var sortColumnDirection = Request.Form["order[0][dir]"];
+
+            IQueryable<Employee> EmployeeQuery;
+            EmployeeQuery = (IQueryable<Employee>)await BussinesService.GetAllAsync(null!);
+
+            if (!string.IsNullOrEmpty(searchValue))
+            {
+                EmployeeQuery = EmployeeQuery.Where(b => b.FullName.Contains(searchValue!)
+                || (b.Court == null || b.Court.Contains(searchValue!))
+                || (b.Appeal == null || b.Appeal.Contains(searchValue!)));
+            }
+
+            var Employee = EmployeeQuery.ToList();
+
+            // Apply sorting in-memory based on known property names
+            if (!string.IsNullOrEmpty(sortColumn) && !string.IsNullOrEmpty(sortColumnDirection))
+            {
+                switch (sortColumn)
+                {
+                    case "FullName":
+                        Employee = sortColumnDirection == "asc" ? Employee.OrderBy(b => b.FullName).ToList() : Employee.OrderByDescending(b => b.FullName).ToList();
+                        break;
+                    case "Court":
+                        Employee = sortColumnDirection == "asc" ? Employee.OrderBy(b => b.Court).ToList() : Employee.OrderByDescending(b => b.Court).ToList();
+                        break;
+                    case "Appeal":
+                        Employee = sortColumnDirection == "asc" ? Employee.OrderBy(b => b.Appeal).ToList() : Employee.OrderByDescending(b => b.Appeal).ToList();
+                        break;
+                    default:
+                        Employee = Employee.OrderByDescending(b => b.DateCreated).ToList(); // Default sorting
+                        break;
+                }
+            }
+            var recordsTotal = Employee.Count;
+            Employee = Employee.ToList();
+            var data = Employee.Skip(skip).Take(pageSize).ToList();
+
+            var mappedData = _mapper.Map<IEnumerable<EmployeeViewModel>>(Employee);
+          
+            var jsonData = new { recordsFiltered = recordsTotal, recordsTotal, data = mappedData };
+
+            return Ok(jsonData);
+        }
+    }
 }
---
+++ StaffWork.Web/wwwroot/js/employees.js
@@ -0,0 +1,75 @@
+﻿$(document).ready(function () {
+    var tbody = $('#Employees').find('tbody');
+    $('[data-kt-filter="search"]').on('keyup', function () {
+        var input = $(this);
+        datatable.search(this.value).draw();
+    });
+
+    let columns = [
+        { "data": "id", "name": "Id", "className": "d-none" },
+        { "data": "fullName", "name": "FullName", "className": "text-center" },
+        { "data": "court", "name": "Court", "className": "text-center" },
+        { "data": "appeal", "name": "Appeal", "className": "text-center" },
+    ];
+    columns.push(
+        {
+            "className": 'text-start',
+            "orderable": false,
+            "render": function (data, type, row) {
+                return `
+                 <a href="javascript:;" class="btn btn-sm btn-outline btn-outline-dashed btn-outline-primary btn-active-light-primary js-render-modal"
+                   data-title="تعديل" data-url="/${tbody.data('controller')}/Edit/${row.id}" data-update="true">
+                    تعديل 
+                </a>`;
+            }
+        });
+
+    columns.push(
+        {
+            "className": 'text-start',
+            "orderable": false,
+            "render": function (data, type, row) {
+                return `
+                <a href="javascript:;" class="btn btn-sm btn-outline btn-outline-dashed btn-outline-danger btn-active-light-danger js-confirm"
+                   data-title="حذف" data-url="/${tbody.data('controller')}/Delete/${row.id}" data-update="true"
+                            data-message="هل متأكد من حذف هذا العنصر؟">
+                    حذف 
+                </a> `;
+            }
+        });
+
+
+   
+
+
+    // Handle title filter change event
+    var titleFilter = $('#titleSearchId');
+    titleFilter.on('change', function () {
+        var selectedTitle = $(this).val();
+        datatable.search(selectedTitle).draw();
+    });
+
+    datatable = $('#Employees').DataTable({
+        serverSide: true,
+        processing: true,
+        stateSave: false,
+        paging: false,
+        ajax: {
+            url: tbody.data('url'),
+            type: 'POST'
+        },
+        language: {
+            emptyTable: 'لا يوجد بيانات فى هذا البحث',
+        },
+        'drawCallback': function () {
+            KTMenu.createInstances();
+        },
+        order: [[1, 'asc']],
+        columnDefs: [{
+            targets: [0],
+            visible: false,
+            searchable: false
+        }],
+        columns: columns,
+    });
+});
